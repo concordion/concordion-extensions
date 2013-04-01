232d1107d621da5248eae96417f00e0a40940118
Nigel Charman <nigel.charman.nz@gmail.com>
2013-04-01 13:51:44 +1300

    Deleted examples and moved core folder to root

--- examples/selenium2/src/test/java/org/concordion/ext/demo/selenium/ExtensionsDemo.java
+++
@@ -1,8 +0,0 @@
-package org.concordion.ext.demo.selenium;
-
-import org.concordion.integration.junit4.ConcordionRunner;
-import org.junit.runner.RunWith;
-
-@RunWith(ConcordionRunner.class)
-public class ExtensionsDemo {
-}
